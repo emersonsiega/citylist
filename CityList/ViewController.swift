39754e9f0314ec8cabbcc648b5adb879259beaf2
Emerson Siega <emersonsiega@hotmail.com>
2023-08-03 23:06:51 -0300

    initial commit

--- CityList/ViewController.swift
+++ CityList/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
+struct Constants {
+    static let cellName = "CityCell"
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var citiesTableView: UITableView!
+    
+    let cities: [String] = [
+        "Rio de Janeiro", "London", "Tokyo", "New York", "Buenos Aires",
+        "Roma", "Barcelona", "Los Angeles", "San Francisco", "Paris",
+    ]
+    let countries: [String:String] = [
+        "Rio de Janeiro": "Brazil", "London": "England", "Tokyo": "Japan", "New York": "EUA",
+        "Buenos Aires": "Argentina", "Roma": "Italy", "Barcelona": "Spain", "Los Angeles": "EUA",
+        "San Francisco": "EUA", "Paris": "France",
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        citiesTableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: Constants.cellName
+        )
+        
+        citiesTableView.dataSource = self
+        citiesTableView.delegate = self
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: Constants.cellName,
+            for: indexPath
+        )
+        
+        var content = cell.defaultContentConfiguration()
+        let city = cities[indexPath.row]
+        content.text = city
+        content.secondaryText = countries[city] ?? "Unknown"
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let cityName = cities[indexPath.row]
+        let country = countries[cityName] ?? ""
+        print("City name \(cityName) at country \(country)")
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# CityList App
+
+Swift course assignment.
+
+### Requirements
+
+- Should present 10 cities with the respective countries in a UITableView
+- The cell text should be the city name and the secondary text the country name
+- Should print the city/country when the user tap the cell
+
+### Result
+
+![](./assets/result.gif)
